410aa9277e8d51b6739bff702baee53997cda5a5
Dale Cloudman <dale.cloudman@gmail.com>
2023-11-19 00:21:24 +0000

    soil depth

--- real_moon.py
+++ real_moon.py
@@ -29,17 +29,19 @@ class MoonModel:
     def __init__(self):
         # start
         self.starting_conditions = {
-            'solar_zenith_angle__deg': 0,
+            # start at horizon
+            'solar_zenith_angle__deg': -90,
 
             # initial soil temp
             # this is set to be 0 joules of energy
-            'soil_temp__K': 50,
+            # we do 200 layers each 1 cm deep
+            'soil_temp__K': 3,
         }
 
         self.vars_logs = {
-            'insolation_W': [],
+            'insolation_W': np.zeros(Constants.moon_day__s),
             # 'earthshine_W': [],
-            'soil_radiation_W': [],
+            'soil_radiation_W': np.zeros(Constants.moon_day__s),
         }
         self.vars_logs_day_means = {
             'insolation_W': [],
@@ -48,15 +50,15 @@ class MoonModel:
         }
 
         # state
-        self.soil_energy__J = 0
-        self.soil_length__m = 1
-        self.soil_width__m = 1
-        self.soil_depth__m = 1
-        self.soil_weight__kg = Constants.soil_density__kg_m3 * (
-            self.soil_length__m *
-            self.soil_width__m *
-            self.soil_depth__m
+        self.soil_layer_length__m = 1
+        self.soil_layer_width__m = 1
+        self.soil_layer_depth__m = 0.1
+        self.soil_layer_weight__kg = Constants.soil_density__kg_m3 * (
+            self.soil_layer_length__m *
+            self.soil_layer_width__m *
+            self.soil_layer_depth__m
         )
+        self.soil_layers_energy__J = [0] * 20
 
         self.sum_dt = 0
         self.steps = 0
@@ -77,17 +79,16 @@ class MoonModel:
 
         return insolation__W_m2 * (1 - Constants.lunar_albedo)
 
-    @property
-    def soil_temp__K(self):
+    def soil_temp__K(self, layer):
         """Get the soil temperature in Kelvin."""
-        temp_change__K = self.soil_energy__J / (self.soil_weight__kg * Constants.soil_specific_heat__J_kgK)
+        temp_change__K = self.soil_layers_energy__J[layer] / (self.soil_layer_weight__kg * Constants.soil_specific_heat__J_kgK)
         return self.starting_conditions['soil_temp__K'] + temp_change__K
 
     @property
     def soil_radiation__W(self):
-        """Get the soil radiation in W."""
-        soil_radiation__W_m2 = Constants.sb_constant__W_m2K4 * self.soil_temp__K**4
-        return soil_radiation__W_m2 * (self.soil_width__m * self.soil_length__m)
+        """Get the soil radiation in W of topmost layer."""
+        soil_radiation__W_m2 = Constants.sb_constant__W_m2K4 * self.soil_temp__K(layer=0)**4
+        return soil_radiation__W_m2 * (self.soil_layer_width__m * self.soil_layer_length__m)
 
     @property
     def elapsed__earth_days(self):
@@ -100,20 +101,38 @@ class MoonModel:
     def step(self, dt=1):
         """Step the model by dt seconds."""
         # this many joules of solar input
-        solar_input__W = self.solar_input__W_m2 * (self.soil_width__m * self.soil_length__m)
+        solar_input__W = self.solar_input__W_m2 * (self.soil_layer_width__m * self.soil_layer_length__m)
         solar_input__J = solar_input__W * dt
 
-        earthshine__W = Constants.earthshine__W_m2 * (self.soil_width__m * self.soil_length__m)
+        earthshine__W = Constants.earthshine__W_m2 * (self.soil_layer_width__m * self.soil_layer_length__m)
         earthshine_input__J = earthshine__W * dt
 
-        self.vars_logs['insolation_W'].append(solar_input__W + earthshine__W)
+        self.vars_logs['insolation_W'][self.steps_day] = solar_input__W + earthshine__W
 
         # soil radiates according to its temperature across its top surface area
         soil_radiation__J = self.soil_radiation__W * dt
-        self.vars_logs['soil_radiation_W'].append(self.soil_radiation__W)
+        self.vars_logs['soil_radiation_W'][self.steps_day] = self.soil_radiation__W
 
         # update variables
-        self.soil_energy__J += solar_input__J + earthshine_input__J - soil_radiation__J
+        # first layer gets the sunlight
+        soil_layers__dJ = [0] * len(self.soil_layers_energy__J)
+        soil_layers__dJ[0] = solar_input__J + earthshine_input__J - soil_radiation__J
+        # rest of layers conduct downward
+        for layer_i in range(1, len(self.soil_layers_energy__J)):
+            # energy conducted is the difference in temperature between this layer and the one below
+            # multiplied by the thermal conductivity of the soil and the distance
+            A = self.soil_layer_width__m * self.soil_layer_length__m
+            dT = self.soil_temp__K(layer=layer_i - 1) - self.soil_temp__K(layer=layer_i)
+            dx = self.soil_layer_depth__m
+            conducted_down__J = Constants.soil_thermal_conductivity__W_mK * A * dT / dx
+            # earlier layer loses
+            soil_layers__dJ[layer_i - 1] -= conducted_down__J
+            # this layer gains
+            soil_layers__dJ[layer_i] += conducted_down__J
+
+        # update the soil values
+        for layer_i in range(len(self.soil_layers_energy__J)):
+            self.soil_layers_energy__J[layer_i] += soil_layers__dJ[layer_i]
 
         # add to total time elapsed
         pre_days = int(self.elapsed__moon_days)
@@ -124,9 +143,9 @@ class MoonModel:
 
         if post_days > pre_days:
             for key, vals in self.vars_logs.items():
-                self.vars_logs_day_means[key].append(int(sum(vals) / len(vals)))
+                self.vars_logs_day_means[key].append(int(round(np.mean(vals))))
                 # clear vars logs
-                self.vars_logs[key] = []
+                self.vars_logs[key] = np.zeros(len(vals))
 
             self.steps_day = 0
 
@@ -136,27 +155,40 @@ class CmdLine:
         soil_temps = []
 
         mm = MoonModel()
-        while mm.elapsed__moon_days < 3:
+        while True:
+            pre_day = int(mm.elapsed__moon_days)
             mm.step()
-            soil_temps.append(mm.soil_temp__K)
-            if mm.steps % 100000 == 0:
-                print("{:.2f}d, soil temp: {:.2f} K, avg insolation: {}, {:.2f}W, avg radiated out: {}, {:.2f} W".format(
+            post_day = int(mm.elapsed__moon_days)
+
+            if mm.steps % 100 == 0:
+                soil_temps.append(mm.soil_temp__K(0))
+
+            if mm.steps % 10000 == 0:
+                print("{:.2f}d, soil temps: [{}]K, avg insolation: {}, {:.2f}W, avg radiated out: {}, {:.2f} W".format(
                     mm.elapsed__moon_days,
-                    mm.soil_temp__K,
+                    " ".join("%.0f" % mm.soil_temp__K(i) for i in range(len(mm.soil_layers_energy__J))),
                     mm.vars_logs_day_means['insolation_W'],
-                    sum(mm.vars_logs['insolation_W']) / (mm.steps_day + 1e-8),
+                    np.mean(mm.vars_logs['insolation_W'][:mm.steps_day]),
                     mm.vars_logs_day_means['soil_radiation_W'],
-                    sum(mm.vars_logs['soil_radiation_W']) / (mm.steps_day + 1e-8),
+                    np.mean(mm.vars_logs['soil_radiation_W'][:mm.steps_day]),
                 ))
 
-        # plot soil temps
-        import matplotlib.pyplot as plt
-        plt.plot(soil_temps)
+            if pre_day != post_day:
+                # plot soil temps
+                import matplotlib.pyplot as plt
+                plt.plot(soil_temps)
 
-        # y axis from 0 to 400
-        plt.ylim(0, 400)
+                # y axis from 0 to 400
+                plt.ylim(0, 400)
 
-        plt.show()
+                # add ticks every
+                plt.yticks(np.arange(0, 400, 20))
+
+                plt.show()
+
+                # clear graph
+                soil_temps = []
+
 
 
 if __name__ == '__main__':
